c7e2b7476d499b59aee78d5aa549ccdd66c586b1
Rohan <rk8222419@gmail.com>
2020-02-18 03:45:41 +0530

    perfected

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ $(document).ready(function(){
         $("#collapsebtn").click(function(){
                 $("#mycollapse").toggleClass('show-menu');
                 $("body").toggleClass("margin-right");
+                $(".overlay").fadeToggle();
         });
 
         $("#collapsebtn2").click(function(){
                 $("#mycollapse").removeClass('show-menu');
                 $("body").toggleClass("margin-right");
+                $(".overlay").fadeToggle();
+        });
+
+        $(".contact").click(function(){
+                $(".sub-menu").fadeToggle();
         });
 });
